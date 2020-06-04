0a8dbdd1fa55d975ff5a84eab8b8ac4340ef1e4a
Anjala <anjaladilhara@gmail.com>
2020-06-04 12:52:30 +0530

    added chnageLocation and refactor

--- lib/services/background_task.dart
+++ lib/services/background_task.dart
@@ -2,106 +2,19 @@ import 'dart:convert';
 
 import 'package:background_fetch/background_fetch.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:semester_5_project_mobile_app/services/notification.dart';
+import 'package:semester_5_project_mobile_app/util/request_handler.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 const EVENTS_KEY = "current_longLat_push";
 
 void backgroundFetchHeadlessTask(String taskId) async {
-  SharedPreferences prefs = await SharedPreferences.getInstance();
-  Position position = await Geolocator()
-      .getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
-  print(position);
   print("[BackgroundFetch] Headless event received: $taskId");
-  String tokenGet = prefs.getString('fcm_token_push');
-  print(tokenGet);
-
-  List<String> events = [];
-  String json = prefs.getString(EVENTS_KEY);
-  if (json != null) {
-    events = jsonDecode(json).cast<String>();
-  }
-  if (events.length == 2) {
-    events.removeLast();
-  }
-  events.insert(0, "$position");
-  prefs.setString(EVENTS_KEY, jsonEncode(events));
-
-  var currLocationList = events[0].split(', ');
-  var currLocation = [
-    currLocationList[0].substring(5),
-    currLocationList[1].substring(6)
-  ];
-  var prevLocation;
-  if (events.length == 2) {
-    var prevLocationList = events[1].split(', ');
-    prevLocation = [
-      prevLocationList[0].substring(5),
-      prevLocationList[1].substring(6)
-    ];
-  }
-
-  var res = {
-    'prev_location': prevLocation,
-    'curr_location': currLocation,
-    'FCM_token': tokenGet
-  };
-  print(res);
+  await _saveLocation();
   BackgroundFetch.finish(taskId);
 }
 
-Future<Map<String, dynamic>> _saveLocation() async {
-  List<String> _events = [];
-
-  SharedPreferences prefs = await SharedPreferences.getInstance();
-  String json = prefs.getString(EVENTS_KEY);
-  if (json != null) {
-    _events = jsonDecode(json).cast<String>();
-  }
-
-  Position position = await Geolocator()
-      .getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
-  print(position);
-
-  if (_events.length == 2) {
-    _events.removeLast();
-  }
-  _events.insert(0, "$position");
-
-  prefs.setString(EVENTS_KEY, jsonEncode(_events));
-  //print(_events);
-  String _fcmToken = prefs.getString('fcm_token_push');
-  print(_fcmToken);
-  var currLocationList = _events[0].split(', ');
-  var currLocation = [
-    currLocationList[0].substring(5),
-    currLocationList[1].substring(6)
-  ];
-
-  var prevLocation;
-
-  if (_events.length == 2) {
-    var prevLocationList = _events[1].split(', ');
-    prevLocation = [
-      prevLocationList[0].substring(5),
-      prevLocationList[1].substring(6)
-    ];
-  }
-
-  var res = {
-    'prev_location': prevLocation,
-    'curr_location': currLocation,
-    'FCM_token': _fcmToken
-  };
-  //send to server
-  return res;
-}
-
 Future<void> initBackgroundTask() async {
-  // Load persisted fetch events from SharedPreferences
-  var l = await _saveLocation();
-  print(l);
-  //send to server
-
   // Configure BackgroundFetch.
   BackgroundFetch.configure(
           BackgroundFetchConfig(
@@ -114,7 +27,7 @@ Future<void> initBackgroundTask() async {
             requiresCharging: false,
             requiresStorageNotLow: false,
             requiresDeviceIdle: false,
-            requiredNetworkType: NetworkType.NONE,
+            requiredNetworkType: NetworkType.ANY,
           ),
           _onBackgroundFetch)
       .then((int status) {
@@ -125,7 +38,62 @@ Future<void> initBackgroundTask() async {
 }
 
 void _onBackgroundFetch(String taskId) async {
-  var l = await _saveLocation();
-  print(l);
+  await _saveLocation();
   BackgroundFetch.finish(taskId);
+}
+
+Future<void> _saveLocation() async {
+  List<String> _events = [];
+
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  if (prefs.containsKey('token') && prefs.containsKey('fcm_token_push')) {
+    String json = prefs.getString(EVENTS_KEY);
+    if (json != null) {
+      _events = jsonDecode(json).cast<String>();
+    }
+
+    Position position = await Geolocator()
+        .getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+    print(position);
+
+    if (_events.length == 2) {
+      _events.removeLast();
+    }
+    _events.insert(0, "$position");
+
+    prefs.setString(EVENTS_KEY, jsonEncode(_events));
+
+    var currLocationList = _events[0].split(', ');
+    var currLocation = [
+      currLocationList[0].substring(5),
+      currLocationList[1].substring(6)
+    ];
+
+    var prevLocation;
+
+    if (_events.length == 2) {
+      var prevLocationList = _events[1].split(', ');
+      prevLocation = [
+        prevLocationList[0].substring(5),
+        prevLocationList[1].substring(6)
+      ];
+    }
+
+    var res = {
+      'prev_location': prevLocation,
+      'curr_location': currLocation,
+      'FCM_token': prefs.getString('fcm_token_push')
+    };
+    bool result = await ApiRequestHandler.updateLocation(
+        data: res, token: prefs.getString('token'));
+    if (result) {
+      NotificationService.showNotification('Commhawk', 'Location updated!');
+    } else {
+      NotificationService.showNotification(
+          'Commhawk', 'Location update failed!');
+    }
+  } else {
+    NotificationService.showNotification(
+        'Commhawk', 'To use commhawk services, Please sign in again.');
+  }
 }
