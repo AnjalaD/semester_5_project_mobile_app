9919fe65268a2bf68a29c07c56f9043cb788868a
Thisun1997 <47599759+Thisun1997@users.noreply.github.com>
2020-08-31 20:38:57 +0530

    label changed and updated nic_validator

--- lib/util/validators/nic_validator.dart
+++ lib/util/validators/nic_validator.dart
@@ -3,7 +3,7 @@ import 'package:semester_5_project_mobile_app/util/validators/regex_validator.da
 class NicValidator extends RegexValidator {
   NicValidator()
       : super(
-          regex: r"^[0-9]{9}(v|V|x)$|^[0-9]{11}$",
+          regex: r"^[0-9]{9}(v|V|x|X)$|^[0-9]{12}$",
           error: 'Invalid nic',
         );
 }
